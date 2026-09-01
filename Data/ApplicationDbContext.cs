9b96b5bf7fb8785881f7c69f4e1773b0d5317177
Pranitha0315 <sriharshanipranitha@gmail.com>
2026-09-01 12:58:42 +0530

    model builder added

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ using EventParkingReservationSystem.API.Models;
 
 namespace EventParkingReservationSystem.API.Data;
 
-public class ApplicationDbContext :DbContext
+public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
@@ -23,33 +23,33 @@ public class ApplicationDbContext :DbContext
     public DbSet<Payment> Payments => Set<Payment>();
     public DbSet<Notification> Notifications => Set<Notification>();
 
-    //protected override void OnModelCreating(ModelBuilder modelBuilder)
-    //{
-    //    base.OnModelCreating(modelBuilder);
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
 
-    //    modelBuilder.Entity<Customer>()
-    //        .HasIndex(x => x.Email)
-    //        .IsUnique();
+        modelBuilder.Entity<Customer>()
+            .HasIndex(x => x.Email)
+            .IsUnique();
 
-    //    modelBuilder.Entity<AdminUser>()
-    //        .HasIndex(x => x.Email)
-    //        .IsUnique();
+        modelBuilder.Entity<AdminUser>()
+            .HasIndex(x => x.Email)
+          .IsUnique();
 
-    //    modelBuilder.Entity<EventCategory>()
-    //        .HasIndex(x => x.Name)
-    //        .IsUnique();
+        modelBuilder.Entity<EventCategory>()
+          .HasIndex(x => x.Name)
+         .IsUnique();
 
-        //modelBuilder.Entity<Seat>()
-        //    .HasIndex(x => new { x.EventId, x.SeatNumber })
-        //    .IsUnique();
+        modelBuilder.Entity<Seat>()
+            .HasIndex(x => new { x.EventId, x.SeatNumber })
+             .IsUnique();
 
-        //modelBuilder.Entity<ParkingSlot>()
-        //    .HasIndex(x => new { x.EventId, x.SlotNumber })
-        //    .IsUnique();
+        modelBuilder.Entity<ParkingSlot>()
+             .HasIndex(x => new { x.EventId, x.SlotNumber })
+          .IsUnique();
 
         //modelBuilder.Entity<Booking>()
-        //    .HasIndex(x => x.BookingNumber)
-        //    .IsUnique();
+        // .HasIndex(x => x.BookingNumber)
+        //.IsUnique();
 
         //modelBuilder.Entity<BookingSeat>()
         //    .HasIndex(x => new { x.BookingId, x.SeatId })
@@ -59,9 +59,9 @@ public class ApplicationDbContext :DbContext
         //    .HasIndex(x => x.BookingId)
         //    .IsUnique();
 
-        //modelBuilder.Entity<Event>().Property(x => x.TicketPrice).HasPrecision(10, 2);
-        //modelBuilder.Entity<Event>().Property(x => x.ParkingFee).HasPrecision(10, 2);
-        //modelBuilder.Entity<Seat>().Property(x => x.Price).HasPrecision(10, 2);
+        modelBuilder.Entity<Event>().Property(x => x.TicketPrice).HasPrecision(10, 2);
+        modelBuilder.Entity<Event>().Property(x => x.ParkingFee).HasPrecision(10, 2);
+        modelBuilder.Entity<Seat>().Property(x => x.Price).HasPrecision(10, 2);
         //modelBuilder.Entity<Booking>().Property(x => x.TotalAmount).HasPrecision(10, 2);
         //modelBuilder.Entity<ParkingReservation>().Property(x => x.FeeAtReservation).HasPrecision(10, 2);
         //modelBuilder.Entity<Payment>().Property(x => x.Amount).HasPrecision(10, 2);
@@ -78,24 +78,25 @@ public class ApplicationDbContext :DbContext
         //    .HasForeignKey(x => x.SeatId)
         //    .OnDelete(DeleteBehavior.Restrict);
 
-        //modelBuilder.Entity<ParkingReservation>()
-        //    .HasOne(x => x.ParkingSlot)
-        //    .WithMany(x => x.ParkingReservations)
-        //    .HasForeignKey(x => x.ParkingSlotId)
-        //    .OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<ParkingReservation>()
+            .HasOne(x => x.ParkingSlot)
+          .WithMany(x => x.ParkingReservations)
+            .HasForeignKey(x => x.ParkingSlotId)
+            .OnDelete(DeleteBehavior.Restrict);
 
-        //modelBuilder.Entity<Event>()
-        //    .HasOne(x => x.Venue)
-        //    .WithMany(x => x.Events)
-        //    .HasForeignKey(x => x.VenueId)
-        //    .OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<Event>()
+           .HasOne(x => x.Venue)
+            .WithMany(x => x.Events)
+            .HasForeignKey(x => x.VenueId)
+            .OnDelete(DeleteBehavior.Restrict);
 
-        //modelBuilder.Entity<Event>()
-        //    .HasOne(x => x.EventCategory)
-        //    .WithMany(x => x.Events)
-        //    .HasForeignKey(x => x.EventCategoryId)
-        //    .OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<Event>()
+            .HasOne(x => x.EventCategory)
+           .WithMany(x => x.Events)
+            .HasForeignKey(x => x.EventCategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
+}
 
 
 
